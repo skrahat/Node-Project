4c528ee8b0bdf4faa5e3e1c5c9d24237a8f4ca37
asaduzzaman rahat <asaduzzaman.rahat@mail.mcgill.ca>
2022-09-16 12:20:35 -0400

    feat: added libraries, test enviornment working

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,16 @@
  */
 
 module.exports = {
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  coverageProvider: "v8",
+  coverageDirectory: "coverage",
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -14,25 +24,16 @@ module.exports = {
   // cacheDirectory: "/private/var/folders/z4/qmjppdzx401ccrc9k17m_ccw0000gn/T/jest_dx",
 
   // Automatically clear mock calls, instances, contexts and results before every test
-  clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
-
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
   //   "/node_modules/"
   // ],
-
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
-
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
   //   "json",
